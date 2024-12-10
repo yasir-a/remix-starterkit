88cd428beb6d4f9b4f38651eded2461eed37c13d
yasir-a <yasir.techhuset@gmail.com>
2024-12-11 04:48:40 +0500

    Installed and set up git hooks for pre-commit checks

--- app/root.tsx
+++ app/root.tsx
@@ -5,15 +5,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import React from "react";
 
 import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Links />
         <Meta />
       </head>
